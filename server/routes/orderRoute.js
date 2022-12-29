58623a0e114300e5bb9cfd37d195421e6b73e2d6
charbel <charbel_makhlouf76@live.com>
2022-12-29 21:42:56 +0200

    card added

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -5,5 +5,6 @@ const orderController = require("../controllers/orderController");
 
 router.post("/newOrder", orderController.createOrder);
 router.get("/getOrders", orderController.getOrders);
+router.patch("/:id/mark-markShipped", orderController.markShipped);
 
 module.exports = router;
